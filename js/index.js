b09a9e21daa13a607d49e8c1a5794fc6c254be63
Siddharth Paneri <dpaneri3@gmail.com>
2023-10-03 22:34:08 +0530

    feat:added animations;scroll effect;

--- js/index.js
+++ js/index.js
@@ -1,34 +1,54 @@
+// for the overlay on the sign-in-up container
 const signInBtn = document.querySelector("#signIn")
 const signUpBtn = document.querySelector("#signUp")
 const overlayRight = document.querySelector(".overlay-right")
 const overlayLeft = document.querySelector(".overlay-left")
 
-const loginBtn = document.querySelector(".login")
-const loginBox = document.querySelector(".login-box-container")
-
-const sendBtn = document.querySelector("#send")
-const resContainer = document.querySelector(".thanks-container")
-const contactBox = document.querySelector(".contact-main-container")
-loginBtn.addEventListener("click", () => {
-    loginBox.classList.toggle("active")
-})
 signInBtn.addEventListener("click", () => {
     overlayLeft.classList.toggle("active")
     overlayRight.classList.toggle("active")
 })
-
 signUpBtn.addEventListener("click", () => {
     overlayRight.classList.toggle("active")
     overlayLeft.classList.toggle("active")
     console.log("hello")
 })
 
+// for showing and hiding the login box
+const loginBtn = document.querySelector(".login")
+const loginBox = document.querySelector(".login-box-container")
+loginBtn.addEventListener("click", () => {
+    loginBox.classList.toggle("active")
+})
+
+// for the contact form switch
+const sendBtn = document.querySelector("#send")
+const resContainer = document.querySelector(".thanks-container")
+const contactBox = document.querySelector(".contact-main-container")
 sendBtn.addEventListener("click", (event) => {
     event.preventDefault()
-    resContainer.style.display = "block"
-    contactBox.style.display = "none"
+    resContainer.classList.toggle("active")
+    contactBox.classList.toggle("not-active")
     setTimeout(() => {
-        resContainer.style.display = "none"
-        contactBox.style.display = "block"
+        resContainer.classList.toggle("active")
+        contactBox.classList.toggle("not-active")
     }, 2000)
 })
+
+// for the reveal on scroll effect
+window.addEventListener("scroll", reveal)
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal")
+    for (let i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight
+        console.log(reveals[i])
+        var revealTop = reveals[i].getBoundingClientRect().top
+        var revealPoint = 150
+        if (revealTop < windowheight - revealPoint) {
+            reveals[i].classList.add("active")
+        }
+        else {
+            reveals[i].classList.remove("active")
+        }
+    }
+}
